e835b7ee70e49f57aa3814adad10e688495a8da1
René <rene.j.spies@gmail.com>
2019-11-26 23:16:45 +0100

    started adding firestore helper which helps to access the firestore more easy and in a more mannered way

---
+++ app/src/main/java/com/aresid/happyapp/FirestoreHelper.java
@@ -0,0 +1,22 @@
+package com.aresid.happyapp;
+
+import android.util.Log;
+
+/**
+ * Created on: 26/11/2019
+ * For Project: HappyApp
+ * Author: René Spies
+ * Copyright: © 2019 Ares ID
+ */
+
+public class FirestoreHelper {
+
+	private static final String TAG = "FirestoreHelper";
+
+	FirestoreHelper() {
+
+		Log.d(TAG, "FirestoreHelper: constructor 0 params");
+
+	}
+
+}
